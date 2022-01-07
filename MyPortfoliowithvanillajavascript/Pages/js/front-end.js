44b5fb2f43d5f61c17a9b63eb37eb825ee1089b3
Antivery <antivery@gmail.com>
2022-01-07 13:20:28 -0500

    folder name change

--- MyPortfoliowithvanillajavascript/Pages/js/front-end.js
+++ MyPortfoliowithvanillajavascript/Pages/js/front-end.js
@@ -61,3 +61,23 @@ if(elem){
 }
  window.addEventListener('scroll', highlightNavItem);
  window.addEventListener('click', highlightNavItem );
+
+ document.querySelector('.contact-form').addEventListener('submit',submitForm);
+ 
+ function submitForm(e){
+    e.preventDefault();
+
+    let  name = document.querySelector('#name').value;
+    let  email =document.querySelector('.email').value;
+    let  phoneNumber = document.querySelector('.phone-number').value;
+    let  message = document.querySelector('#message').value;
+
+    saveContactInfo(name, email, phoneNumber,message);
+
+    document.querySelector('.contact-form').reset();
+ }
+
+
+ 
+
+ 
