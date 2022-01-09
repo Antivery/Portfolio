4209667aa1760aa1e67dfe51061c538450844d03
Antivery <antivery@gmail.com>
2022-01-09 14:23:32 -0500

    db connection made

--- MyPortfoliowithvanillajavascript/Pages/server.js
+++ MyPortfoliowithvanillajavascript/Pages/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var fs = require("fs")
+var mongoose = require('mongoose')
+var app = express();    
 
-var app = express();
+var dbUrl = 'mongodb+srv://Antivery:Bigant513@cluster0.2dxmi.mongodb.net/PortfolioSite?retryWrites=true&w=majority'
+
+
+mongoose.connect(dbUrl,(err) => {
+    console.log('connected' , err);
+})
 
 app.use(express.static(__dirname))
 app.listen(3000);
