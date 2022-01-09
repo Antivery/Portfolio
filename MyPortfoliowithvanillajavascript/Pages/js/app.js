50b9d5ab663f0aea97f7e84169961d9db6a3e5ec
Antivery <antivery@gmail.com>
2022-01-09 17:46:20 -0500

    server requiest test

--- MyPortfoliowithvanillajavascript/Pages/js/app.js
+++ MyPortfoliowithvanillajavascript/Pages/js/app.js
@@ -1,4 +1,20 @@
-fs = require('fs');
+$(() => {
+    $('#send').click((event) =>{
+        event.preventDefault();
+        addMessages({name:"Anthony", message:"hi"})
+    })
+         
+    getMessages()
+})
+
+function addMessages(message){
+    $("#message-div").append(`<h3>${message.message}</h3> <p>${message.name}</p>`)
+}
+
+function getMessages(){
+$.get("http://localhost:3000/messages",(data) =>{
+    data.forEach(addMessages)
+});
 
 
-fs
+}
--- MyPortfoliowithvanillajavascript/Pages/server.js
+++ MyPortfoliowithvanillajavascript/Pages/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require("fs")
 var mongoose = require('mongoose')
 var app = express();    
-
+var bodyParser = require("body-parser")
 var dbUrl = 'mongodb+srv://Antivery:Bigant513@cluster0.2dxmi.mongodb.net/PortfolioSite?retryWrites=true&w=majority'
 
 
@@ -10,5 +10,21 @@ mongoose.connect(dbUrl,(err) => {
     console.log('connected' , err);
 })
 
+
+var messages =[{
+   name: "me",
+   message:"keep bossing up" 
+},{
+    name: "Dess",
+    message:"I love you" 
+ } ] 
+app.get("/messages", (req, res) =>{
+    res.send(messages)
+})
+app.post("/messages", (req, res) =>{
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+app.use(bodyParser.json())
 app.use(express.static(__dirname))
 app.listen(3000);
