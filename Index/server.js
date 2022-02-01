8189a253343eacc32ea957efeab242b53dd648a5
Antivery <41758165+Antivery@users.noreply.github.com>
2022-02-01 09:15:36 -0500

    .

--- Index/server.js
+++ Index/server.js
@@ -128,7 +128,7 @@ app.post("/",[
 // })
 
 
-const server = process.env.SERVER_PORT || 8080
+const server = process.env.PORT || 8080
 
 http.listen(server, () =>{
     console.log('server is listening to port ' + server)
